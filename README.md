435057c7339215f2f962367fae048552e4bc7a1f
Kai Yan <kyan8@uci.edu>
2022-05-15 19:47:17 -0700

    Replace OpenEXR by TinyEXR

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Path-space differentiable renderer
 
 Official documentation: https://psdr-cuda.readthedocs.io/en/latest/
+
+Update:
+5/15/2022: psdr-cuda no longer requires OpenEXR, which is hard to build/link by many users. We are using TinyEXR
--- src/core/bitmap_loader.cpp
+++ src/core/bitmap_loader.cpp
@@ -1,54 +1,55 @@
-#ifdef _WIN32
-#   define OPENEXR_DLL
-#endif
-#include <ImathBox.h>
-#include <ImfArray.h>
-#include <ImfRgba.h>
-#include <ImfRgbaFile.h>
 #include <psdr/psdr.h>
 #include <psdr/core/bitmap_loader.h>
+#include <misc/Exception.h>
+
+#define TINYEXR_USE_MINIZ 0
+#include <psdr/core/miniz.h>
+#define TINYEXR_IMPLEMENTATION
+#include <psdr/core/tinyexr.h>
 
 namespace psdr
 {
 
 std::pair<Vector4fC, ScalarVector2i> BitmapLoader::load_openexr_rgba(const char *file_name) {
-    Imf::RgbaInputFile file(file_name);
-    Imath::Box2i dw = file.dataWindow();
+    {
+        float* out; // width * height * RGBA
+        int width;
+        int height;
+        const char* err = NULL; // or nullptr in C++11
 
-    Imf::Array2D<Imf::Rgba> pixels;
+        int ret = LoadEXR(&out, &width, &height, file_name, &err);
+        int size = width*height;
 
-    int width = dw.max.x - dw.min.x + 1;
-    int height = dw.max.y - dw.min.y + 1;
-    pixels.resizeErase(height, width);
-    file.setFrameBuffer(&pixels[0][0] - dw.min.x - dw.min.y * width, 1, width);
-    file.readPixels(dw.min.y, dw.max.y);
+        if (ret != TINYEXR_SUCCESS) {
+            if (err) {
+            fprintf(stderr, "ERR : %s\n", err);
+               FreeEXRErrorMessage(err); // release memory of error message.
+               PSDR_ASSERT(0);
+            }
+          } 
+        std::vector<float> buf[4];
+        for ( int i = 0; i < 4; ++i ) buf[i].resize(size);
 
-    // std::cout << width << ' ' << height << std::endl;
-    int size = width*height;
-
-    std::vector<float> buf[4];
-    for ( int i = 0; i < 4; ++i ) buf[i].resize(size);
-
-
-    int offset = 0;
-    for ( int i = 0; i < height; ++i )
-        for ( int j = 0; j < width; ++j ) {
-            buf[0][offset] = pixels[i][j].r;
-            buf[1][offset] = pixels[i][j].g;
-            buf[2][offset] = pixels[i][j].b;
-            buf[3][offset] = pixels[i][j].a;
-            ++offset;
-        }
-
-    return {
-        Vector4fC(
-            FloatC::copy(buf[0].data(), size),
-            FloatC::copy(buf[1].data(), size),
-            FloatC::copy(buf[2].data(), size),
-            FloatC::copy(buf[3].data(), size)
-        ),
-        ScalarVector2i(width, height)
-    };
+        int offset = 0;
+        for ( int i = 0; i < height; ++i )
+            for ( int j = 0; j < width; ++j ) {
+                buf[0][offset] = out[offset*4];
+                buf[1][offset] = out[offset*4+1];
+                buf[2][offset] = out[offset*4+2];
+                buf[3][offset] = out[offset*4+3];
+                ++offset;
+            }
+        free(out); // release memory of image data
+        return {
+            Vector4fC(
+                FloatC::copy(buf[0].data(), size),
+                FloatC::copy(buf[1].data(), size),
+                FloatC::copy(buf[2].data(), size),
+                FloatC::copy(buf[3].data(), size)
+            ),
+            ScalarVector2i(width, height)
+        }; 
+    }
 }
 
 } // namespace psdr
